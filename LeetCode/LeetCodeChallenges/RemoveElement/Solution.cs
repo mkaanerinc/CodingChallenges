20d6c53e06f1f0af26adbb1b2735821dcf873226
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-05-08 13:25:05 +0300

    Added Remove Element

---
+++ LeetCode/LeetCodeChallenges/RemoveElement/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeChallenges.RemoveElement;
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
---
+++ LeetCode/LeetCodeChallengesTests/RemoveElement/SolutionTest.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeChallenges.RemoveElement;
+
+namespace LeetCodeChallengesTests.RemoveElement;
+
+public class SolutionTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenArray_WhenRemoveElement_ThenResultAsExpected(int[] nums, int val, int expectedValue)
+    {
+        int k = new Solution().RemoveElement(nums, val);
+        k.ShouldBe(expectedValue);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return
+        [
+            new[] { 3,2,2,3 },
+            3,
+            2,
+        ];
+        yield return
+        [
+            new[] { 0,1,2,2,3,0,4,2 },
+            2,
+            5,
+        ];
+    }
+}
