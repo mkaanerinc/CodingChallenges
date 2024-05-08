3136776413d586fd36577f52d2f61cc829fdf1d2
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-05-08 13:55:01 +0300

    Added Remove Duplicates From Sorted Array

---
+++ LeetCode/LeetCodeChallenges/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeChallenges.RemoveDuplicatesFromSortedArray;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 0;
+        int isUnique = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (isUnique != nums[i] || k == 0)
+            {
+                nums[k] = nums[i];
+                k++;
+                isUnique = nums[i];
+            }
+        }
+        return k;
+    }
+}
---
+++ LeetCode/LeetCodeChallengesTests/RemoveDuplicatesFromSortedArray/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeChallenges.RemoveDuplicatesFromSortedArray;
+
+namespace LeetCodeChallengesTests.RemoveDuplicatesFromSortedArray;
+
+public class SolutionTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenArray_WhenRemoveDuplicatesNumbers_ThenResultAsExpected(int[] nums, int expectedValue)
+    {
+        int k = new Solution().RemoveDuplicates(nums);
+        k.ShouldBe(expectedValue);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return
+        [
+            new[] { 1,1,2 },
+            2,
+        ];
+        yield return
+        [
+            new[] { 0,0,1,1,1,2,2,3,3,4 },
+            5,
+        ];
+    }
+}
